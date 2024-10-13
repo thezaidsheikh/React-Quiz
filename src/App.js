53523b49ea009a25cdddb1b9316d0725faa40f82
Zaid Sheikh <zaidsheikh530@gmail.com>
2024-10-13 19:29:58 +0530

    Add first page of react quiz app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
 
+const initialState = {
+  questions: [],
+  status: "Loading",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+      break;
+    case "dataFailed":
+      return { ...state, status: "error" };
+    default:
+      break;
+  }
+};
 function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numberOfQuestions = questions.length;
+  useEffect(function () {
+    fetch(`http://localhost:9000/question`)
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: "dataReceived", payload: data });
+      })
+      .catch((error) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Main>
+        {status == "Loading" && <Loader />}
+        {status == "error" && <Error />}
+        {status == "ready" && <StartScreen numberOfQuestions={numberOfQuestions} />}
+      </Main>
     </div>
   );
 }
